1a6270960880903074a813923cc845e41dd23fa8
SEOKKAMONI <sj01048260279@gmail.com>
2023-10-18 20:03:34 +0900

    chore: theme 세팅

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -29,7 +29,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
+function Section({ children, title }: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -46,6 +46,7 @@ function Section({children, title}: SectionProps): JSX.Element {
         style={[
           styles.sectionDescription,
           {
+            fontFamily: 'NanumDdobakDdobak',
             color: isDarkMode ? Colors.light : Colors.dark,
           },
         ]}>
